7c96518a5ed074ce018a530073339979b4e7868f
Colder Xihk <cncolder@gmail.com>
2014-08-25 02:16:55 +0800

    config for my self.

--- shells/bash/lib/~colder.bash
+++ shells/bash/lib/~colder.bash
@@ -1,6 +1,17 @@
 #!/bin/bash
 #
 # ~colder.bash -- Bash support for myself.
+# 
+# This script use for myself. Start with '~' to ensure it's the last script. So that I can overwrite the others.
+
+# It make this things:
+# 1. Replace prompt with ☃ because I'm Colder.
+# 2. Set default editor as mate if there is.
+# 3. Add ~/.usr/bin to PATH.
+# 4. Active rvm
+# 5. Active nvm.
+# 6. Setup narwhal.
+
 #
 
 if [[ "$USER" != "colder" && "$USER" != "mes" ]]; then
