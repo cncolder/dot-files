8ac2193431740789ca6fa670dd339f72e1f49ef1
colder <cncolder@gmail.com>
2014-11-28 14:59:00 +0800

    move config from .bashrc to ~colder.bash

--- shells/bash/lib/~colder.bash
+++ shells/bash/lib/~colder.bash
@@ -4,13 +4,12 @@
 # 
 # This script use for myself. Start with '~' to ensure it's the last script. So that I can overwrite the others.
 
-# It make this things:
+# It make these things:
 # 1. Replace prompt with ☃ because I'm Colder.
 # 2. Set default editor as mate if there is.
 # 3. Add ~/.usr/bin to PATH.
 # 4. Active rvm
 # 5. Active nvm.
-# 6. Setup narwhal.
 
 #
 
@@ -23,51 +22,27 @@ function precmd() {
     PS1="$(user_at_host)$(prompt_pwd)☃ "
 }
 
-# Use for ruby 1.9 encoding.
-export LC_CTYPE=en_US.UTF-8
-
-# export CLICOLOR="cons25"
-
-# Compile as 64bit. Mac os x has upgrade to 64bit after snow leopard.
-export ARCHFLAGS="-arch x86_64"
-
 # My soft bin path. To avoid sudo(/usr/local).
-export PATH=$HOME"/.usr/bin":$PATH
+export PATH=$HOME/.usr/bin:$PATH
 
 # Set default editor.
-# if editor="$(which mvim)"; [[ -s $editor ]]; then
-#   export EDITOR="$editor -f"
-#   export VISUAL="$editor"
-#   export GEM_OPEN_EDITOR="$editor"
-#   export GIT_EDITOR="$editor -f"
-# el
 if editor="$(which mate)"; [[ -s $editor ]]; then
   export EDITOR="$editor"
   export VISUAL="$editor"
   export GEM_OPEN_EDITOR="$editor"
-  export GIT_="$editor"
+  export GIT_EDITOR="$editor"
 fi
 
 # Active ruby version manager.
-# rvm=$HOME"/.rvm/scripts/rvm" && [[ -s $rvm ]] && . $rvm
+rvm=$HOME/.rvm/scripts/rvm && [[ -s $rvm ]] && . $rvm # Load RVM into a shell session *as a function*
+export PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
 
 # Active node version manager.
-nvm=$HOME"/.nvm/nvm.sh" && [[ -s $nvm ]] && . $nvm
-export PATH="node_modules/.bin":$PATH
+nvm=$HOME/.nvm/nvm.sh && [[ -s $nvm ]] && . $nvm
+export PATH=node_modules/.bin:$PATH
+
+### Added by the Heroku Toolbelt
+export PATH=/usr/local/heroku/bin:$PATH
 
 # n
-# export N_PREFIX=$HOME"/.usr"
-
-# FIXME: This script is too slowly. Wait 280north upgrade it.
-# Active narwhal enviriment.
-# activate=$HOME"/.usr/narwhal/bin/activate" && [[ -s $activate ]] && . $activate
-# export NARWHAL_ENGINE=jsc
-# export CAPP_BUILD="$HOME/Documents/Github/cappuccino/Build"
-#
-# export DYLD_LIBRARY_PATH="/usr/local/oracle/instantclient_10_2"
-# export PATH=$DYLD_LIBRARY_PATH:$PATH
-# export SQLPATH="/usr/local/oracle/instantclient_10_2"
-# export TNS_ADMIN="/usr/local/oracle/network/admin"
-# export NLS_LANG="AMERICAN_AMERICA.UTF8"
-
-# export PATH="/Applications/eclipse/sdks/android-sdk-mac_86":$PATH
+# export N_PREFIX=$HOME/.usr
